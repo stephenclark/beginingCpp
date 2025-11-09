78b83970523d8926f5c6cd8de1eaf74239670650
stephenclark <stephen.k.clark@gmail.com>
2025-11-09 15:24:49 +1000

    section_5

--- section_4/src/main.cpp
+++ section_4/src/main.cpp
@@ -8,9 +8,7 @@ int main(){
     int favorite_number;
     std::cout << "Enter your favorite number between 1 and 100: ";
     std::cin >> favorite_number;
-    std::cout << "Enter your favorite number between 1 and 100: ";
-
-    int error_number = 1;
-    std::cout << error_number / 0 << std::endl;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    std::cout << "No, really "<< favorite_number << " is my fav number" <<std::endl;
     return 0;
 }
